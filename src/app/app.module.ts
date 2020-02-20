02524487a2fd7aaab35f61ac895cf0e0f97ea90d
RjHuffaker <freedomsworn@gmail.com>
2020-02-20 11:53:24 -0700

    Added Modal and NavBar Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, NavBarComponent, ModalComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
