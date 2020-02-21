76076203509858852e9d19547e64e7def08cfb80
RjHuffaker <freedomsworn@gmail.com>
2020-02-21 10:29:07 -0700

    2 files modified, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppComponent } from './app.component'
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, NavBarComponent, ModalComponent ],
+  declarations: [ AppComponent, NavBarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
